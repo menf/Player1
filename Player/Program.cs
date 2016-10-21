489943a1a64ef1bf300e8d40495393bfd87e44d0
menf <menfis666@gmail.com>
2016-10-21 23:32:21 +0200

    testy muzy

--- Player/Program.cs
+++ Player/Program.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSCore.Codecs;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using System.Collections.ObjectModel;
 
 namespace Player
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
+            /// testy dzialania
 
+            Logic _musicPlayer = new Logic();
+            ObservableCollection<MMDevice> _devices = new ObservableCollection<MMDevice>();
+            using (var mmdeviceEnumerator = new MMDeviceEnumerator())
+            {
+                using (
+                    var mmdeviceCollection = mmdeviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active))
+                {
+                    foreach (var device in mmdeviceCollection)
+                    {
+                        _devices.Add(device);
+                        Console.WriteLine(device);
+                    }
+                }
+
+
+                _musicPlayer.Open(Console.ReadLine(), _devices[0]);
+                if (_musicPlayer.PlaybackState != PlaybackState.Playing)
+                {
+                    _musicPlayer.Play();
+                }
+
+                }
+
+            Console.ReadLine();
         }
-
     }
 }
 
